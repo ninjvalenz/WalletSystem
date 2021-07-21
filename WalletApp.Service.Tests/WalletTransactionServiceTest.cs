f4d4b15e4369d513decaf4c8adde5e681f2b4f51
Janine Valenzona <jvalenzona@besixgroup.com>
2021-07-21 12:28:35 +0800

    fix unit test project

---
+++ WalletApp.Service.Tests/WalletTransactionServiceTest.cs
@@ -0,0 +1,33 @@
+﻿using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WalletApp.Service.ConnectionStrings;
+using WalletApp.Service.Interface;
+
+namespace WalletApp.Service.Tests
+{
+    [TestFixture]
+    public class WalletTransactionServiceTest
+    {
+        private Mock<ISequelConnection> dbConnection;
+
+        [SetUp]
+        public void Setup()
+        {
+            dbConnection = new Mock<ISequelConnection>();
+        }
+
+        private IWalletTransactionService GetUserSecurityService()
+        {
+            return new WalletTransactionService(dbConnection.Object);
+        }
+
+        [Test]
+        public void DepositMoney_Returns_DepositMoneyViewModel()
+        {
+
+        }
+    }
+}
