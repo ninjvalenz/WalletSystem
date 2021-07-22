a781eb7cbdd5b73d9ffbbef461ca699592445a3e
Janine Valenzona <jvalenzona@besixgroup.com>
2021-07-22 19:01:13 +0800

    wallet transac unit tests

--- WalletApp.Controller.Tests/WalletControllerTest.cs
+++ WalletApp.Controller.Tests/WalletControllerTest.cs
@@ -54,14 +54,14 @@ namespace WalletApp.Controller.Tests
             var controller = GetWalletController();
 
             //Arrange
-            dbWalletTransactionService.Setup(x => x.DepositMoney(It.IsAny<long>(), -1).Result)
+            dbWalletTransactionService.Setup(x => x.DepositMoney(It.IsAny<long>(), 999999999999).Result)
                 .Returns(GenerateDepositMoneyViewModel_MaximumAllowableAmountException());
 
             //Act
             var result = controller.Deposit(new Model.ViewModel.RequestBodyModel.TransactionViewModel()
             {
                 AccountNumber = 111111111111,
-                Amount = -1
+                Amount = 999999999999
             }).Result;
 
             //Assert
@@ -96,7 +96,7 @@ namespace WalletApp.Controller.Tests
             var controller = GetWalletController();
 
             //Arrange
-            dbWalletTransactionService.Setup(x => x.DepositMoney(It.IsAny<long>(), It.IsAny<decimal>()).Result)
+            dbWalletTransactionService.Setup(x => x.DepositMoney(It.IsAny<long>(), 100).Result)
                 .Returns(new DepositMoneyViewModel());
 
             //Act
@@ -115,6 +115,239 @@ namespace WalletApp.Controller.Tests
 
         #endregion
 
+        #region Withdraw
+
+        [Test]
+        public void WalletController_Withdraw_Returns_BadRequest_TooLowAmountException()
+        {
+            var controller = GetWalletController();
+
+            //Arrange
+            dbWalletTransactionService.Setup(x => x.WithdrawMoney(It.IsAny<long>(), -1).Result)
+               .Returns(GenerateWithdrawMoneyViewModel_TooLowAmountException());
+
+            //Act
+            var result = controller.Withdraw(new Model.ViewModel.RequestBodyModel.TransactionViewModel()
+            {
+                AccountNumber = 111111111111,
+                Amount = -1
+            }).Result;
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.IsInstanceOf<IActionResult>(result);
+            Assert.AreEqual(((ObjectResult)result).Value, new TooLowAmountException().Message);
+
+        }
+
+        [Test]
+        public void WalletController_Withdraw_Returns_BadRequest_MaximumAllowableAmountException()
+        {
+            var controller = GetWalletController();
+
+            //Arrange
+            dbWalletTransactionService.Setup(x => x.WithdrawMoney(It.IsAny<long>(), 999999999999).Result)
+                .Returns(GenerateWithdrawMoneyViewModel_MaximumAllowableAmountException());
+
+            //Act
+            var result = controller.Withdraw(new Model.ViewModel.RequestBodyModel.TransactionViewModel()
+            {
+                AccountNumber = 111111111111,
+                Amount = 999999999999
+            }).Result;
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.IsInstanceOf<IActionResult>(result);
+            Assert.AreEqual(((ObjectResult)result).Value, new MaximumAllowableAmountException().Message);
+
+        }
+
+        [Test]
+        public void WalletController_Withdraw_Returns_BadRequest_InsufficientWalletBalanceException()
+        {
+            var controller = GetWalletController();
+
+            //Arrange
+            dbWalletTransactionService.Setup(x => x.WithdrawMoney(It.IsAny<long>(), 100).Result)
+                .Returns(GenerateWithdrawMoneyViewModel_InsufficientWalletBalanceException());
+
+            //Act
+            var result = controller.Withdraw(new Model.ViewModel.RequestBodyModel.TransactionViewModel()
+            {
+                AccountNumber = 111111111111,
+                Amount = 100
+            }).Result;
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.IsInstanceOf<IActionResult>(result);
+            Assert.AreEqual(((ObjectResult)result).Value, new InsufficientWalletBalanceException().Message);
+
+        }
+
+        [Test]
+        public void WalletController_Withdraw_Returns_BadRequest_RequiredFieldsException()
+        {
+            var controller = GetWalletController();
+
+            //Arrange
+            dbWalletTransactionService.Setup(x => x.WithdrawMoney(It.IsAny<long>(), -1).Result)
+                .Returns(new WithdrawMoneyViewModel());
+
+            //Act
+            var result = controller.Withdraw(new Model.ViewModel.RequestBodyModel.TransactionViewModel() { }).Result;
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.IsInstanceOf<IActionResult>(result);
+            Assert.AreEqual(((ObjectResult)result).Value, new RequiredFieldsException().Message);
+
+        }
+
+        [Test]
+        public void WalletController_Withdraw_Returns_Ok()
+        {
+            var controller = GetWalletController();
+
+            //Arrange
+            dbWalletTransactionService.Setup(x => x.WithdrawMoney(It.IsAny<long>(), 100).Result)
+                .Returns(new WithdrawMoneyViewModel());
+
+            //Act
+            var result = controller.Withdraw(new Model.ViewModel.RequestBodyModel.TransactionViewModel()
+            {
+                AccountNumber = 111111111111,
+                Amount = 100
+            }).Result;
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.IsInstanceOf<IActionResult>(result);
+            Assert.AreEqual(((ObjectResult)result).Value.GetType(), typeof(WithdrawMoneyViewModel));
+
+        }
+
+        #endregion
+
+        #region Transfer
+
+        [Test]
+        public void WalletController_Transfer_Returns_BadRequest_TooLowAmountException()
+        {
+            var controller = GetWalletController();
+
+            //Arrange
+            dbWalletTransactionService.Setup(x => x.TransferMoney(It.IsAny<long>(), It.IsAny<long>(), -1).Result)
+               .Returns(GenerateTransferMoneyViewModel_TooLowAmountException());
+
+            //Act
+            var result = controller.Transfer(new Model.ViewModel.RequestBodyModel.NewTransferViewModel()
+            {
+                AccountNumber = 111111111111,
+                ToAccountNumber = 222222222222,
+                Amount = -1
+            }).Result;
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.IsInstanceOf<IActionResult>(result);
+            Assert.AreEqual(((ObjectResult)result).Value, new TooLowAmountException().Message);
+
+        }
+
+        [Test]
+        public void WalletController_Transfer_Returns_BadRequest_MaximumAllowableAmountException()
+        {
+            var controller = GetWalletController();
+
+            //Arrange
+            dbWalletTransactionService.Setup(x => x.TransferMoney(It.IsAny<long>(), It.IsAny<long>(), 999999999999).Result)
+                .Returns(GenerateTransferMoneyViewModel_MaximumAllowableAmountException());
+
+            //Act
+            var result = controller.Transfer(new Model.ViewModel.RequestBodyModel.NewTransferViewModel()
+            {
+                AccountNumber = 111111111111,
+                ToAccountNumber = 222222222222,
+                Amount = 999999999999
+            }).Result;
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.IsInstanceOf<IActionResult>(result);
+            Assert.AreEqual(((ObjectResult)result).Value, new MaximumAllowableAmountException().Message);
+
+        }
+
+        [Test]
+        public void WalletController_Transfer_Returns_BadRequest_InsufficientWalletBalanceException()
+        {
+            var controller = GetWalletController();
+
+            //Arrange
+            dbWalletTransactionService.Setup(x => x.TransferMoney(It.IsAny<long>(), It.IsAny<long>(), 100).Result)
+                .Returns(GenerateTransferMoneyViewModel_InsufficientWalletBalanceException());
+
+            //Act
+            var result = controller.Transfer(new Model.ViewModel.RequestBodyModel.NewTransferViewModel()
+            {
+                AccountNumber = 111111111111,
+                ToAccountNumber = 222222222222,
+                Amount = 100
+            }).Result;
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.IsInstanceOf<IActionResult>(result);
+            Assert.AreEqual(((ObjectResult)result).Value, new InsufficientWalletBalanceException().Message);
+
+        }
+
+        [Test]
+        public void WalletController_Transfer_Returns_BadRequest_RequiredFieldsException()
+        {
+            var controller = GetWalletController();
+
+            //Arrange
+            dbWalletTransactionService.Setup(x => x.TransferMoney(It.IsAny<long>(), It.IsAny<long>(), -1).Result)
+                .Returns(new TransferMoneyViewModel());
+
+            //Act
+            var result = controller.Transfer(new Model.ViewModel.RequestBodyModel.NewTransferViewModel() { }).Result;
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.IsInstanceOf<IActionResult>(result);
+            Assert.AreEqual(((ObjectResult)result).Value, new RequiredFieldsException().Message);
+
+        }
+
+        [Test]
+        public void WalletController_Transfer_Returns_Ok()
+        {
+            var controller = GetWalletController();
+
+            //Arrange
+            dbWalletTransactionService.Setup(x => x.TransferMoney(It.IsAny<long>(), It.IsAny<long>(), 100).Result)
+                .Returns(new TransferMoneyViewModel());
+
+            //Act
+            var result = controller.Transfer(new Model.ViewModel.RequestBodyModel.NewTransferViewModel()
+            {
+                AccountNumber = 111111111111,
+                ToAccountNumber = 222222222222,
+                Amount = 100
+            }).Result;
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.IsInstanceOf<IActionResult>(result);
+            Assert.AreEqual(((ObjectResult)result).Value.GetType(), typeof(TransferMoneyViewModel));
+
+        }
+
+        #endregion
 
         private WalletController GetWalletController()
         {
@@ -138,6 +371,54 @@ namespace WalletApp.Controller.Tests
             return new DepositMoneyViewModel()
             {
                 Message = new MaximumAllowableAmountException().Message
+            };
+        }
+
+        private WithdrawMoneyViewModel GenerateWithdrawMoneyViewModel_TooLowAmountException()
+        {
+            return new WithdrawMoneyViewModel()
+            {
+                Message = new TooLowAmountException().Message
+            };
+        }
+
+        private WithdrawMoneyViewModel GenerateWithdrawMoneyViewModel_MaximumAllowableAmountException()
+        {
+            return new WithdrawMoneyViewModel()
+            {
+                Message = new MaximumAllowableAmountException().Message
+            };
+        }
+
+        private WithdrawMoneyViewModel GenerateWithdrawMoneyViewModel_InsufficientWalletBalanceException()
+        {
+            return new WithdrawMoneyViewModel()
+            {
+                Message = new InsufficientWalletBalanceException().Message
+            };
+        }
+
+        private TransferMoneyViewModel GenerateTransferMoneyViewModel_TooLowAmountException()
+        {
+            return new TransferMoneyViewModel()
+            {
+                Message = new TooLowAmountException().Message
+            };
+        }
+
+        private TransferMoneyViewModel GenerateTransferMoneyViewModel_MaximumAllowableAmountException()
+        {
+            return new TransferMoneyViewModel()
+            {
+                Message = new MaximumAllowableAmountException().Message
+            };
+        }
+
+        private TransferMoneyViewModel GenerateTransferMoneyViewModel_InsufficientWalletBalanceException()
+        {
+            return new TransferMoneyViewModel()
+            {
+                Message = new InsufficientWalletBalanceException().Message
             };
         }
 
